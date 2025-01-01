3a5f165dd00cda6afa10965fd63a5f8797700cf2
JongSeongYang <yangjs0523@naver.com>
2025-01-01 16:47:26 +0900

    Initial commit

---
+++ src/test/java/com/jongseong/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongseong.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
